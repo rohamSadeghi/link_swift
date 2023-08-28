94994a9292af97ac3b3e85ba4127c510a799d775
Roham <roham.sa88@gmail.com>
2023-08-28 23:24:32 +0200

    Move test_url to setUp

--- url_shortener/api/tests/test_views.py
+++ url_shortener/api/tests/test_views.py
@@ -8,33 +8,30 @@ from url_shortener.models import ShortenedURL
 class TestUrlShortenerAPI(APITestCase):
     def setUp(self):
         self.test_client = APIClient()
+        self.test_url = "https://www.test.com"
 
     def test_if_create_short_url_is_successful(self):
         url = api_reverse('urls-shorten')
-        test_url = "https://www.test.com"
-        res = self.test_client.post(path=url, data={"url": test_url})
-        obj = ShortenedURL.objects.get(url=test_url)
+        res = self.test_client.post(path=url, data={"url": self.test_url})
+        obj = ShortenedURL.objects.get(url=self.test_url)
         self.assertContains(res, obj.short_code, status_code=status.HTTP_201_CREATED)
 
     def test_invalid_url_for_creating_short_code_returns_error(self):
         url = api_reverse('urls-shorten')
-        test_url = "test"
-        res = self.test_client.post(path=url, data={"url": test_url})
+        res = self.test_client.post(path=url, data={"url": "test"})
         self.assertEqual(ShortenedURL.objects.count(), 0)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_if_existing_shortcode_request_returns_existing_shortcode(self):
         url = api_reverse('urls-shorten')
-        test_url = "https://www.test.com"
-        self.test_client.post(path=url, data={"url": test_url})
-        obj = ShortenedURL.objects.get(url=test_url)
-        res = self.test_client.post(path=url, data={"url": test_url})
+        self.test_client.post(path=url, data={"url": self.test_url})
+        obj = ShortenedURL.objects.get(url=self.test_url)
+        res = self.test_client.post(path=url, data={"url": self.test_url})
         self.assertContains(res, obj.short_code, status_code=status.HTTP_303_SEE_OTHER)
         self.assertEqual(ShortenedURL.objects.count(), 1)
 
     def test_if_request_shortcode_detail_view_increases_number_of_hits(self):
-        test_url = "https://www.test.com"
-        obj = ShortenedURL.objects.create(url=test_url)
+        obj = ShortenedURL.objects.create(url=self.test_url)
         detail_url = api_reverse("urls-detail", kwargs={"short_code": obj.short_code})
         self.assertEqual(obj.hits, 0)
 
@@ -45,8 +42,7 @@ class TestUrlShortenerAPI(APITestCase):
         self.assertEqual(obj.hits, 4)
 
     def test_if_stats_page_request_is_successfull(self):
-        test_url = "https://www.test.com"
-        obj = ShortenedURL.objects.create(url=test_url)
+        obj = ShortenedURL.objects.create(url=self.test_url)
         stats_url = api_reverse("urls-stats", kwargs={"short_code": obj.short_code})
         res = self.test_client.get(path=stats_url)
         self.assertContains(res, obj.hits, status_code=status.HTTP_200_OK)
